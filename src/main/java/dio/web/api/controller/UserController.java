8bf1b6fa10f8bcb2814c6091cd4242542fc6d6af
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-06 11:58:11 -0300

    feat: adds getUser, deleteUser and createUser controllers

--- src/main/java/dio/web/api/controller/UserController.java
+++ src/main/java/dio/web/api/controller/UserController.java
@@ -3,17 +3,29 @@ package dio.web.api.controller;
 import dio.web.api.model.User;
 import dio.web.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
-    @GetMapping("/users")
+    @GetMapping()
     public List<User> listUsers() {
         return userRepository.findAll();
+    }
+    @GetMapping("/{userName}")
+    public User getUser(@PathVariable("userName") String userName) {
+        return userRepository.findByName(userName);
+    }
+    @DeleteMapping("/delete/{id")
+    public void deleteUser(@PathVariable("id") Integer id) {
+        userRepository.deleteById(id);
+    }
+    @PostMapping()
+    public void createUser(@RequestBody User user) {
+        userRepository.save(user);
     }
 }
